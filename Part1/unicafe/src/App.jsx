1ed6da02c212d613d9f780a0d39fbfb12e177294
danielpuliche <danielpuliche@unicauca.edu.co>
2024-08-15 16:04:14 -0500

    7th Exercise - 1hour

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -8,10 +8,45 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState("0%");
 
-  const handleClickGood = () => setGood(good + 1);
-  const handleClickNeutral = () => setNeutral(neutral + 1);
-  const handleClickBad = () => setBad(bad + 1);
+  const handleClickGood = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    const updatedAll = updatedGood + neutral + bad;
+    setAll(updatedAll);
+    calculateAverage(updatedGood, bad, updatedAll);
+  };
+
+  const handleClickNeutral = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    const updatedAll = good + updatedNeutral + bad;
+    setAll(updatedAll);
+    calculateAverage(good, bad, updatedAll);
+  };
+
+  const handleClickBad = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    const updatedAll = good + neutral + updatedBad;
+    setAll(updatedAll);
+    calculateAverage(good, updatedBad, updatedAll);
+  };
+
+  const calculateAverage = (updatedGood, updatedBad, updatedAll) => {
+    const sum = updatedGood - updatedBad;
+    const avg = sum / updatedAll;
+    setAverage(avg);
+    calculatePositive(updatedGood, updatedAll);
+  };
+
+  const calculatePositive = (updatedGood, updatedAll) => {
+    const positiveRate = 100 * (updatedGood / updatedAll);
+    setPositive(positiveRate +  " %");
+  };
 
   return (
     <div>
@@ -22,10 +57,13 @@ function App() {
         <Button handleClick={handleClickBad} text={"Bad"} />
       </div>
       <div>
-        <Title title={"Statistics"}/>
-        <Stat text={"Good"} value={good}/>
-        <Stat text={"Neutral"} value={neutral}/>
-        <Stat text={"Bad"} value={bad}/>
+        <Title title={"Statistics"} />
+        <Stat text={"Good"} value={good} />
+        <Stat text={"Neutral"} value={neutral} />
+        <Stat text={"Bad"} value={bad} />
+        <Stat text={"All"} value={all} />
+        <Stat text={"Average"} value={average} />
+        <Stat text={"Positive"} value={positive} />
       </div>
     </div>
   );
