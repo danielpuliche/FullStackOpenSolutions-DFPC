e3d49ed69cbe990794fc12818294da0c9aeb1bd6
danielpuliche <danielpuliche@unicauca.edu.co>
2024-08-15 16:27:37 -0500

    9th Exercise - 0.2hour

--- Part1/unicafe/src/components/Statistics.jsx
+++ Part1/unicafe/src/components/Statistics.jsx
@@ -2,17 +2,26 @@ import Stat from "./Stat";
 import Title from "./Title";
 
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  return (
-    <div>
-      <Title title={"Statistics"} />
-      <Stat text={"Good"} value={good} />
-      <Stat text={"Neutral"} value={neutral} />
-      <Stat text={"Bad"} value={bad} />
-      <Stat text={"All"} value={all} />
-      <Stat text={"Average"} value={average} />
-      <Stat text={"Positive"} value={positive} />
-    </div>
-  );
+  if (all === 0) {
+    return (
+      <div>
+        <Title title={"Statistics"} />
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Title title={"Statistics"} />
+        <Stat text={"Good"} value={good} />
+        <Stat text={"Neutral"} value={neutral} />
+        <Stat text={"Bad"} value={bad} />
+        <Stat text={"All"} value={all} />
+        <Stat text={"Average"} value={average} />
+        <Stat text={"Positive"} value={positive} />
+      </div>
+    );
+  }
 };
 
 export default Statistics;
