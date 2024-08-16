91ad1b4d7fcaa42c8b456a59d153cdae6cd78eef
danielpuliche <danielpuliche@unicauca.edu.co>
2024-08-16 17:12:38 -0500

    Part2 - 4th and 5th exercises - 0.6hour

--- Part2/courseinfo/src/components/Course.jsx
+++ Part2/courseinfo/src/components/Course.jsx
@@ -2,11 +2,12 @@ import Header from "./Header";
 import Content from "./Content";
 
 const Course = ({ course }) => {
+  console.log(course.name);
   return (
-    <>
+    <div>
       <Header text={course.name} />
       <Content parts={course.parts} />
-    </>
+    </div>
   );
 };
 
