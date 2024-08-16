2e15987b32b73516ccdd39ed5704985bf9b56bd6
danielpuliche <danielpuliche@unicauca.edu.co>
2024-08-16 16:34:44 -0500

    Part2 - 2nd exercise - 0.2hour

--- Part2/courseinfo/src/components/Content.jsx
+++ Part2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,19 @@
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ parts }) => {
+  let total = 0;
+
+  for (let i = 0; i < parts.length; i++) {
+    total += parts[i].exercises;
+  }
+
   return (
     <div>
       {parts.map((part) => (
         <Part key={part.id} text={part.name} value={part.exercises} />
       ))}
+      <Total total={total} />
     </div>
   );
 };
