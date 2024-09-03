aec2115e64b0b3f5de20c20b3a4feccdf6a0e985
danielpuliche <danielpuliche@unicauca.edu.co>
2024-09-03 00:47:42 -0500

    Part3 - 15th-18th exercises - 1.5hours

--- Part3/phonebook-backend/index.js
+++ Part3/phonebook-backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const morgan = require("morgan");
 const cors = require("cors");
 
 // Get Person schema from MongoDB and get connection
 const Person = require("./models/person");
+
+app.use(express.static("./dist"));
+app.use(cors());
+app.use(express.json());
 
 // Token for the personalized POST format
 morgan.token("body", (req) => {
@@ -13,10 +18,6 @@ morgan.token("body", (req) => {
 
 const postFormat =
   ":method :url :status :res[content-length] - :response-time ms - :body";
-
-app.use(express.static("./dist"));
-app.use(express.json());
-app.use(cors());
 
 // Set the format for the morgan middleware
 app.use((req, res, next) => {
@@ -30,68 +31,100 @@ app.use((req, res, next) => {
 // ===================================================================================
 // Routes
 
-// app.get("/info", (request, response) => {
-//   const currentDate = new Date();
-//   response.send(
-//     `<p>Phonebook has info for ${persons.length} people</p><p>${currentDate}</p>`
-//   );
-// });
-
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => response.json(persons));
-});
-
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id)
-    .then((person) => {
-      response.json(person);
+app.get("/info", (req, res, next) => {
+  Person.countDocuments({})
+    .then((count) => {
+      const currentDate = new Date();
+      res.send(
+        `<p>Phonebook has info for ${count} people</p><p>${currentDate}</p>`
+      );
     })
-    .catch((error) => {
-      response.status(404).json({ error: "Person not found" });
-    });
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => res.json(persons))
+    .catch((error) => next(error));
+});
+
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/persons", (req, res, next) => {
   const { name, number } = req.body;
 
-  if (!name || !number) {
+  if (name === undefined || number === undefined) {
     return res.status(400).json({
       error: "Name and number are required fields.",
     });
   }
-
-  // const nameExists = persons.some((person) => person.name === name.trim());
-
-  // if (nameExists) {
-  //   return res.status(409).json({
-  //     error: "Name must be unique",
-  //   });
-
-  // }
 
   const person = new Person({
     name: name,
     number: number,
   });
 
-  person.save().then((savedPerson) => {
-    res.status(201).json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      res.status(201).json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
 
-  if (persons.some((person) => person.id === id)) {
-    const deletedPerson = persons.find((person) => person.id === id);
-    persons = persons.filter((person) => person.id !== id);
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-    console.log(deletedPerson);
-    response.status(200).json(deletedPerson);
-  } else {
-    response.status(404).end();
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((deletedPerson) => {
+      res.status(200).json(deletedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+// ===================================================================================
+// For unknown endpoints
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "Uknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+// ===================================================================================
+// Error handler
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "Malformatted id" });
   }
-});
+  next(error);
+};
+
+app.use(errorHandler);
 
 // ===================================================================================
 // Run the server
