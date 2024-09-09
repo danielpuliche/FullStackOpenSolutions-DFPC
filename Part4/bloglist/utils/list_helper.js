0795428613d6dee3e7b08ee6998e2aa422383273
danielpuliche <danielpuliche@unicauca.edu.co>
2024-09-09 17:26:55 -0500

    Part4 - 5th exercise - 1hour

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) { return null }
+  return blogs.reduce((favoriteBlogObj, blog) => {
+    return blog.likes > favoriteBlogObj.likes ? blog : favoriteBlogObj
+  }, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
