62da7f74907f9cbdd2c9185fd4c608baa5beb4a5
danielpuliche <danielpuliche@unicauca.edu.co>
2025-02-13 16:42:54 -0500

    Part5 - 10th exercise - 0.2 hours

--- Part5/bloglist-frontend/src/components/BlogList.jsx
+++ Part5/bloglist-frontend/src/components/BlogList.jsx
@@ -1,9 +1,13 @@
 import Blog from './Blog'
 
-const BlogList = ({ blogs, likeBlog }) => (
-  <div>
-    {blogs.map(blog => <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />)}
-  </div>
-)
+const BlogList = ({ blogs, likeBlog }) => {
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes)
+
+  return (
+    <div>
+      {sortedBlogs.map(blog => <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />)}
+    </div>
+  )
+}
 
 export default BlogList
