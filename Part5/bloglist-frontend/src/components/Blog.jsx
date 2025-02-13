29ce74a8b182972014aff459cb116e5a0e666858
danielpuliche <danielpuliche@unicauca.edu.co>
2025-02-13 13:14:47 -0500

    Part5 - 7th exercise - 0.6 hours

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <p>
-    {blog.title} {blog.author}
-  </p>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        "{blog.title}" by {blog.author} <button onClick={toggleVisibility}>View</button>
+      </div>
+      <div style={showWhenVisible}>
+        "{blog.title}" by {blog.author} <button onClick={toggleVisibility}>Hide</button><br />
+        Url: {blog.url}<br />
+        likes: {blog.likes} <button>Like</button><br />
+        Added by: {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
